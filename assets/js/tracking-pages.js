81347a2d2793555c85237799a16735625052010f
elyon27 <avelar2231@gmail.com>
2026-03-31 12:30:11 +0900

    update

--- assets/js/tracking-pages.js
+++ assets/js/tracking-pages.js
@@ -4,5 +4,15 @@ window.trackingPages = {
   "You Build Your World.pdf": "../../assets/articles/html/you-build-your-world.html",
   "You Are More Than Life.pdf": "../../assets/articles/html/you-are-more-than-life.html",
   "Why Call Me Lord.pdf": "../../assets/articles/html/why-call-me-lord.html",
-  "When You Are Misunderstood.pdf": "../../assets/articles/html/when-you-are-misunderstood.html"
+  "When You Are Misunderstood.pdf": "../../assets/articles/html/when-you-are-misunderstood.html",
+  "When Silence Is Strength.pdf": "../../assets/articles/html/when-silence-is-strength.html",
+  "Tuning The Mind.pdf": "../../assets/articles/html/tuning-the-mind.html",
+  "Thorn in the Flesh.pdf": "../../assets/articles/html/thorn-in-the-flesh.html",
+  "They Look But Not See.pdf": "../../assets/articles/html/they-look-but-not-see.html",
+  "There Is Hope.pdf": "../../assets/articles/html/there-is-hope.html",
+  "The Tower Of Babel.pdf": "../../assets/articles/html/the-tower-of-babel.html",
+  "The Refining Fire.pdf": "../../assets/articles/html/the-refining-fire.html",
+  "The Power Of Love.pdf": "../../assets/articles/html/the-power-of-love.html",
+  "The Kingdom Within.pdf": "../../assets/articles/html/the-kingdom-within.html",
+  "The Inner Sanctuary.pdf": "../../assets/articles/html/the-inner-sanctuary.html"
 };
